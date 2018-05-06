80fd4cd9ac373442fc1bcfbf189036ea9c270406
David Aguilar <davidaguilarh.94@gmail.com>
2018-05-06 17:39:52 -0500

    Set routes for the initial aplication stage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :coworkers
-  resources :tours
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tours, only: [:index, :show]
+  resources :coworkers, only: :show
 end
