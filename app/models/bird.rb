f96f9b2d7763eef8c41871f8c8c0e4dc22f8ff03
David Aguilar <davidaguilarh.94@gmail.com>
2018-05-21 00:11:48 -0500

    Add Birds crud

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,6 +1,14 @@
 class Bird < ApplicationRecord
   has_one_attached :profile_image
+  has_many_attached :specie_images
 
   validates :popular_name, presence: true
   validates :cientific_name, presence: true
+  validates :description, presence: true
+  validates :status, presence: true
+  validates :size, presence: true
+
+  enum status: [:Extinct, :Extinct_in_the_Wild, :Critically_Endangered, :Endangered, :Vulnerable, :Near_Threatened, :Least_Concern, :Data_Deficient, :Not_Evaluated]
+
+  enum size: [:small, :medium, :large]
 end
